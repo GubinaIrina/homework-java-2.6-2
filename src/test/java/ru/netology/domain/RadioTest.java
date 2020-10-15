f8684440f61f49447ab1a204b8bb986dcf8f1d12
Gubina Irina <irina.gubina98@mail.ru>
2020-10-15 12:55:23 +0300

    Обновлен RadioTest.java

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -29,10 +29,38 @@ class RadioTest {
         radio.increaseRadioStation();
         System.out.println(radio.getRadioStationCurrent());
     }
+    @Test
+    void increaseRadioStationBeforeMax(){
+        Radio radio = new Radio();
+        radio.setRadioStationCurrent(8);
+        radio.increaseRadioStation();
+        System.out.println(radio.getRadioStationCurrent());
+    }
+    @Test
+    void increaseRadioStationUnderMax(){
+        Radio radio = new Radio();
+        radio.setRadioStationCurrent(9);
+        radio.increaseRadioStation();
+        System.out.println(radio.getRadioStationCurrent());
+    }
 
     @Test
     void decreaseRadioStation(){
         Radio radio = new Radio();
+        radio.decreaseRadioStation();
+        System.out.println(radio.getRadioStationCurrent());
+    }
+    @Test
+    void decreaseRadioStationUnderMin(){
+        Radio radio = new Radio();
+        radio.setRadioStationCurrent(1);
+        radio.decreaseRadioStation();
+        System.out.println(radio.getRadioStationCurrent());
+    }
+    @Test
+    void decreaseRadioStationBeforeMin(){
+        Radio radio = new Radio();
+        radio.setRadioStationCurrent(0);
         radio.decreaseRadioStation();
         System.out.println(radio.getRadioStationCurrent());
     }
@@ -43,10 +71,38 @@ class RadioTest {
         radio.increaseVolume();
         System.out.println(radio.getVolumeCurrent());
     }
+    @Test
+    void increaseVolumeBeforeMax(){
+        Radio radio = new Radio();
+        radio.setVolumeCurrent(9);
+        radio.increaseVolume();
+        System.out.println(radio.getVolumeCurrent());
+    }
+    @Test
+    void increaseVolumeUnderMax(){
+        Radio radio = new Radio();
+        radio.setVolumeCurrent(10);
+        radio.increaseVolume();
+        System.out.println(radio.getVolumeCurrent());
+    }
 
     @Test
     void decreaseVolume(){
         Radio radio = new Radio();
+        radio.decreaseVolume();
+        System.out.println(radio.getVolumeCurrent());
+    }
+    @Test
+    void decreaseVolumeBeforeMin(){
+        Radio radio = new Radio();
+        radio.setVolumeCurrent(0);
+        radio.decreaseVolume();
+        System.out.println(radio.getVolumeCurrent());
+    }
+    @Test
+    void decreaseVolumeUnderMin(){
+        Radio radio = new Radio();
+        radio.setVolumeCurrent(1);
         radio.decreaseVolume();
         System.out.println(radio.getVolumeCurrent());
     }
